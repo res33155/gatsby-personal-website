baf76a7b6b66de0407eb16602aacb9d006df9c30
Ryan Smith <1578766+ryan3E0@users.noreply.github.com>
2020-07-25 22:00:09 -0400

    Add hand wave animation

--- src/components/page-sections/hero/hero.js
+++ src/components/page-sections/hero/hero.js
@@ -23,13 +23,55 @@ const Paragraph = styled.p`
   color: var(--blue-200);
 `
 
+const HandWave = styled.span`
+  animation-name: wave-animation; 
+  animation-duration: 2.5s;
+  animation-iteration-count: 1;
+  transform-origin: 70% 70%;
+  display: inline-block;
+
+  @keyframes wave-animation {
+    0% {
+      transform: rotate( 0.0deg) 
+    }
+    10% {
+      transform: rotate(14.0deg)
+    }
+    20% {
+      transform: rotate(-8.0deg) 
+    }
+    30% {
+      transform: rotate(14.0deg) 
+    }
+    40% {
+      transform: rotate(-4.0deg) 
+    }
+    50% {
+      transform: rotate(10.0deg) 
+    }
+    60% {
+      transform: rotate(0.0deg) 
+    }
+    100% {
+      transform: rotate(0.0deg)
+    }
+  }
+`
+
 const Hero = () => {
   return (
     <>
       <HeroContainer>
         <Container>
           <Heading>
-            <span role="img" aria-label="Hand waving">👋</span> Hi, I'm Ryan
+            <HandWave>
+              <span
+                role="img"
+                aria-label="Hand waving"
+              >
+                👋
+              </span>
+            </HandWave> Hi, I'm Ryan
           </Heading>
 
           <Paragraph>
